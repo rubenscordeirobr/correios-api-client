30bcc7b0e6c74ee827d5e8842e80b0837beaf30f
Rubens Cordeiro <zyoncore@hotmail.com>
2023-09-24 14:34:25 -0300

    added apis

---
+++ src/schemas/apis.ts
@@ -0,0 +1,12 @@
+export enum Apis {
+    //https://api.correios.com.br/token/v3/api-docs
+    Token = 5,
+    //https://api.correios.com.br/preco/v3/api-docs
+    Preco = 34,
+    //https://api.correios.com.br/prazo/v3/api-docs
+    Prazo= 35,
+    //https://api.correios.com.br/cep/v3/api-docs
+    Cep = 41,
+    //https://api.correios.com.br/srorastro/v3/api-docs
+    Rastro = 87
+}
