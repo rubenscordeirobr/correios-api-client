c9933d5d233835adf52a87c5ad0d715ad3fada4d
Rubens Cordeiro <zyoncore@hotmail.com>
2023-09-24 16:39:22 -0300

    added lastToken Config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-import { Ambientes, Configuration as Configuration } from "./schemas/";
+import { Ambientes, Configuration as Configuration, Token } from "./schemas/";
 
 const UrlApi = "https://api.correios.com.br";
 const UrlApiHomologacao = "https://apihom.correios.com.br";
@@ -8,27 +8,47 @@ class ConfigSingleton {
     private _isInitialized: boolean = false;
     private _contrato?: string;
     private _cartaoPostagem?: string;
+    private _lastToken?: Token;
     private ambiente?: Ambientes;
     private idCorreios?: string;
     private codigoAcesso?: string;
 
-    public get contrato(): string {
+    /**
+     * The contract number used for making requests to the Correios API.
+     * @throws An error if the contract has not been initialized with the `config.initialize` method.
+     */
+    get contrato(): string {
         this.checkIsInitialized();
         if (!this._contrato)
             throw new Error("O contrato não foi inicializado, utilize o método config.initialize");
         return this._contrato;
     }
 
-    public get CartaoPostagem(): string | null {
+    /**
+     * Gets the Cartão de Postagem (posting card) number associated with the Correios account.
+     * 
+     * @returns The Cartão de Postagem number, or `null` if it has not been set.
+     * @throws {Error} If the client has not been initialized yet.
+     */
+    get CartaoPostagem(): string | null {
         this.checkIsInitialized();
         return this._cartaoPostagem ?? null;
     }
 
+    /**
+     * Returns the URL of the API to be used based on the current environment.
+     * @returns The URL of the API.
+     */
     get urlApi(): string {
         this.checkIsInitialized();
         return this.ambiente == Ambientes.Producao ? UrlApi : UrlApiHomologacao;
     }
 
+    /**
+     * Returns the basic authentication string for the Correios API client.
+     * @throws {Error} If the authentication settings have not been initialized.
+     * @returns {string} The basic authentication string.
+     */
     get basicAuth(): string {
         this.checkIsInitialized();
 
@@ -36,7 +56,21 @@ class ConfigSingleton {
             throw new Error("As configurações de autenticação não foram inicializadas, utilize o método config.initialize");
         return `Basic ${Buffer.from(`${this.idCorreios}:${this.codigoAcesso}`).toString("base64")}`;
     }
- 
+
+    /**
+     * Returns the last token used to authenticate with the Correios API.
+     * If no token has been set, returns null.
+     * @returns {Token | null} The last token used to authenticate with the Correios API, or null if no token has been set.
+     */
+    get lastToken(): Token | null {
+        this.checkIsInitialized();
+        return this._lastToken ?? null;
+    }
+
+    /**
+     * Initializes the configuration object with the provided values.
+     * @param init - The configuration object to initialize with.
+     */
     initialize(init: Configuration) {
         this.ambiente = init.ambiente;
         this.idCorreios = init.idCorreios;
@@ -46,9 +80,24 @@ class ConfigSingleton {
         this._isInitialized = true;
     }
 
-    public checkIsInitialized() {
+    /**
+     * Checks if the configuration for Correios API client has been initialized.
+     * Throws an error if it has not been initialized.
+     */
+    checkIsInitialized() {
         if (!this._isInitialized)
             throw new Error("As configuração dos correios não foi inicializada, utilize o método config.initialize");
+    }
+
+    /**
+     * Sets the last token used for authentication.
+     * @param token The token to set.
+     * @throws An error if the token is not defined.
+     */
+    setLastToken(token: Token) {
+        if (!token)
+            throw new Error("Token is not defined");
+        this._lastToken = token;
     }
 
 }
@@ -56,21 +105,25 @@ class ConfigSingleton {
 class Config {
 
     public static instance: ConfigSingleton;
- 
-    public get urlApi(): string {
+
+    get urlApi(): string {
         return Config.instance.urlApi;
     }
 
-    public get basicAuth(): string {
+    get basicAuth(): string {
         return Config.instance.basicAuth;
     }
- 
-    public get contrato(): string {
+
+    get contrato(): string {
         return Config.instance.contrato;
     }
 
-    public get cartaoPostagem(): string | null {
+    get cartaoPostagem(): string | null {
         return Config.instance.CartaoPostagem;
+    }
+
+    get lastToken(): Token | null {
+        return Config.instance.lastToken;
     }
 
     constructor() {
@@ -86,6 +139,10 @@ class Config {
 
     checkIsInitialized() {
         Config.instance.checkIsInitialized();
+    }
+
+    setLastToken(token: Token) {
+        Config.instance.setLastToken(token);
     }
 }
 
