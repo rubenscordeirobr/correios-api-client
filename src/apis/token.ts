2e3343963cba7796fb422665c6022a51d9a55079
Rubens Cordeiro <zyoncore@hotmail.com>
2023-09-24 16:36:49 -0300

    refactored TokenClient

--- src/apis/token.ts
+++ src/apis/token.ts
@@ -1,8 +1,8 @@
 import config from "../config";
 import { HttpClient } from "../utils/http-client";
-import { MessageResponse, Token } from "../schemas/";
+import { CorreiosAPIs, MessageResponse, Token } from "../schemas/";
 
-const endpoint = "/token/v1";
+const TokenEndpoint = "/token/v1";
 
 /**
  * TokenClient is a class that provides methods to authenticate and generate tokens for accessing APIs.
@@ -24,7 +24,10 @@ const endpoint = "/token/v1";
  * If you exceed this limit, HTTP status 429 - Too Many Requests is triggered.
  * To avoid receiving HTTP status 429, always check the expiration date of the token before sending a new request.
  */
-export class TokenClient {
+export abstract class BaseTokenClient {
+
+    protected abstract readonly CurrentApi: CorreiosAPIs;
+    protected abstract readonly Endpoint: string;
 
     constructor() {
     }
@@ -34,10 +37,8 @@ export class TokenClient {
      * @returns A Promise that resolves to a Token object if the authentication is successful, or a MessageResponse object if there was an error.
      */
     async autentica(): Promise<Token | MessageResponse> {
-        // Check if the configuration has been initialized
         config.checkIsInitialized();
-        // Send a POST request to the API to authenticate and generate a token
-        return this.post(`/autentica`);
+        return this.autenticaInternal(`/autentica`);
     }
 
     /**
@@ -51,7 +52,7 @@ export class TokenClient {
             numero: numero
         }
         // Send a POST request to the API to authenticate and generate a token
-        return this.post(`/autentica/contrato`, body);
+        return this.autenticaInternal(`/autentica/contrato`, body);
     }
 
     /**
@@ -68,8 +69,34 @@ export class TokenClient {
         const body = {
             numero: numero
         }
-        // Send a POST request to the API to authenticate and generate a token
-        return this.post(`/autentica/cartaopostagem`, body);
+        return this.autenticaInternal(`/autentica/cartaopostagem`, body);
+    }
+
+    /**
+     * Authenticates the user and returns a token or a message response.
+     * @param path - The path to authenticate the user.
+     * @param body - The body of the request.
+     * @returns A promise that resolves to a Token object or a MessageResponse object.
+     */
+    async autenticaInternal(path: string, body?: any): Promise<Token | MessageResponse> {
+
+        const result = await this.post(`${TokenEndpoint}${path}`, body);
+        if (result instanceof Token) {
+            config.setLastToken(result);
+
+            if (this.CurrentApi !== CorreiosAPIs.Token && result.api != null && result.api.indexOf(this.CurrentApi) == -1)
+                console.error("The Cep API is not authorized in the current token");
+        }
+        return result;
+    }
+
+    /**
+     * Validates the token for the specified API. If a new token is needed, it will be obtained by calling the `autenticaContrato` method.
+     * @param apiId The ID of the API to validate the token for.
+     */
+    protected async validateToken(): Promise<void> {
+        if (this.isNeedNewToken())
+            await this.generateNewToken();
     }
 
     /**
@@ -78,20 +105,13 @@ export class TokenClient {
      * @param body The request body.
      * @returns A Promise that resolves to a Token object if the authentication is successful, or a MessageResponse object if there was an error.
      */
-    private async post(path: string, body?: any): Promise<Token | MessageResponse> {
+    protected async post(path: string, body?: any): Promise<any> {
 
         try {
-            const fullUrl = `${config.urlApi}${endpoint}${path}`;
-            // Send a POST request to the API with the specified path, basic authentication, and request body
+            const fullUrl = `${config.urlApi}${path}`;
             const result = await HttpClient.post(fullUrl, config.basicAuth, body);
-            // Check if the result is a Token or a MessageResponse
-            if (this.isToken(result))
-                return new Token(result);
-
-            if (this.isMessageResponse(result))
-                return new MessageResponse(result);
-
-            throw new Error(`Unknown result: ${JSON.stringify(result)}`)
+            return this.normalizeResult(result);
+            // throw new Error(`Unknown result: ${JSON.stringify(result)}`)
         }
         catch (erro: any) {
             // If an error occurs, return a MessageResponse object with the error details
@@ -104,6 +124,51 @@ export class TokenClient {
                 stackTrace: erro.stackTrace
             });
         }
+    }
+
+    /**
+     * Sends a GET request to the specified URL with the authorization token.
+     * @param fullUrl - The full URL to send the GET request to.
+     * @returns A Promise that resolves with the response data of type T.
+     * @throws An error if the token is not defined or the Cep API is not authorized in the current token.
+     */
+    protected async get(path: string): Promise<any> {
+
+        await this.validateToken();
+
+        const lastToken = config.lastToken;
+        if (lastToken == null || lastToken.token == null)
+            throw new Error("Token is not defined");
+
+
+        try {
+            const fullUrl = `${config.urlApi}${path}`;
+            const bearerToken = `Bearer ${lastToken.token}`;
+            const result = await HttpClient.get(fullUrl, bearerToken);
+            return this.normalizeResult(result);
+        }
+        catch (erro: any) {
+            return new MessageResponse({
+                msgs: [erro?.message ?? "Unknown catch error"],
+                path: path,
+                method: "POST",
+                date: new Date().toISOString(),
+                causa: "Catch error",
+                stackTrace: erro.stackTrace
+            });
+        }
+
+    }
+
+
+    private normalizeResult(result: any): any {
+        if (this.isToken(result))
+            return new Token(result);
+
+        if (this.isMessageResponse(result))
+            return new MessageResponse(result);
+
+        return result
     }
 
     /**
@@ -123,4 +188,49 @@ export class TokenClient {
     private isMessageResponse(obj: any): obj is MessageResponse {
         return obj.msgs !== undefined;
     }
+
+    /**
+     * Determines whether a new token is needed for the specified API.
+     * @param apiId The ID of the API to check.
+     * @returns True if a new token is needed, false otherwise.
+     */
+    private isNeedNewToken(): boolean {
+        return config.lastToken == null ||
+            config.lastToken.expiraEm == null ||
+            new Date(config.lastToken.expiraEm) < new Date() ||
+            config.lastToken.api?.indexOf(this.CurrentApi) == -1;
+    }
+
+    /**
+     * Generates a new token for authentication.
+     * If a cartaoPostagem is set in the config, it will authenticate using that.
+     * If a contrato is set in the config, it will authenticate using that.
+     * Otherwise, it will authenticate using the default credentials.
+     */
+    private async generateNewToken(): Promise<void> {
+
+        config.checkIsInitialized();
+
+        if (config.cartaoPostagem != null) {
+            console.log("autenticaCartaoPostagem");
+            await this.autenticaCartaoPostagem();
+            return;
+        }
+
+        if (config.contrato != null) {
+            console.log("autenticaContrato");
+            await this.autenticaContrato();
+        }
+
+        console.log("autentica");
+        await this.autentica();
+    }
 }
+
+export class TokenClient extends BaseTokenClient {
+
+    protected override readonly CurrentApi: CorreiosAPIs = CorreiosAPIs.Token;
+    protected override readonly Endpoint = TokenEndpoint;
+}
+
+
