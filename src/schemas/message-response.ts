900a190a2186ca1d2f740bb7e39bd2c409a0799b
Rubens Cordeiro <zyoncore@hotmail.com>
2023-09-24 14:35:34 -0300

    Added MessageResponse

---
+++ src/schemas/message-response.ts
@@ -0,0 +1,19 @@
+
+export class MessageResponse {
+
+    readonly causa: string;
+    readonly date: string;
+    readonly method: string;
+    readonly msgs: string[];
+    readonly path: string;
+    readonly stackTrace: string;
+
+    public constructor(init: MessageResponse) {
+        this.causa = init.causa;
+        this.date = init.date;
+        this.method = init.method;
+        this.msgs = init.msgs;
+        this.path = init.path;
+        this.stackTrace = init.stackTrace;
+    }
+}
