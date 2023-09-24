2d5db8c12921f7206eb586e0d6d97adc8a5924d0
Rubens Cordeiro <zyoncore@hotmail.com>
2023-09-24 14:36:07 -0300

    added schemas/index

---
+++ src/schemas/index.ts
@@ -0,0 +1,9 @@
+export  * from './ambientes';
+export  * from './apis';
+export  * from './cartao-postagem';
+export  * from './contrato';
+export  * from './token';
+export  * from './configuration';
+export  * from './message-response';
+ 
+
