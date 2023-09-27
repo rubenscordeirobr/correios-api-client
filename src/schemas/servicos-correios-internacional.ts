c53d2190b73f7d8176024d04dfb59aa1242df17a
Rubens Cordeiro <zyoncore@hotmail.com>
2023-09-27 20:32:49 -0300

    added enum ServicosCorreiosInternacional

---
+++ src/schemas/servicos-correios-internacional.ts
@@ -0,0 +1,20 @@
+/**
+ * Enum representing the available international services provided by Correios.
+ */
+export enum  ServicosCorreiosInternacional {
+    CartaResposta_Internacional_Convencional = "13013",
+    CartaoResposta_Internacional_Convencional = "15016",
+    MalaDireta_Economica = "29041",
+    MalaDireta_Economica_EUA_Canada = "29050",
+    Documento_Internacional_Expresso = "45012",
+    Documento_Internacional_Standard = "45039",
+    Documento_Internacional_Standard_Chanc = "45071",
+    ExportaFacil_Express = "45110",
+    ExportaFacil_Standard = "45128",
+    Documento_Internacional_Premium = "45179",
+    ExportaFacil_Premium = "45195",
+    ExportaFacil_Economico = "45209",
+    Telegrama_ViaInternet_Internacional_Ordinaria = "63037",
+    Telegrama_ViaInternet_Interna = "63053",
+    CartaMundial_ViaInternet_Internacional = "69230",
+}
